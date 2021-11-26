61b865ff2aff7eebe65c6fde9529efe673de6e1f
AmbujaAK <itsambuja@gmail.com>
2021-11-27 02:20:42 +0530

    added encryption and decryption

--- src/main/java/com/itsambuja/nuchange/service/ApiServiceImpl.java
+++ src/main/java/com/itsambuja/nuchange/service/ApiServiceImpl.java
@@ -1,32 +1,32 @@
 package com.itsambuja.nuchange.service;
 
-import com.itsambuja.nuchange.model.Store;
-import com.itsambuja.nuchange.util.Helper;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.itsambuja.nuchange.model.Store;
+import com.itsambuja.nuchange.util.Helper;
 
 @Service
-public class ApiServiceImpl implements  ApiService {
+public class ApiServiceImpl implements ApiService {
     public static List<Store> localStorage = new ArrayList<Store>();
 
-    @Override
     public ResponseEntity<String> storeUrl(String url) {
         boolean isNew = true;
 
         try {
             for (Store st : localStorage) {
-                if (st.getKey().equals(url)) {
+                if (Helper.decrypt(st.getKey()).equals(url)) {
                     st.setCount(st.getCount() + 1);
                     isNew = false;
                 }
             }
 
             if (isNew) {
-                localStorage.add(new Store(url, 1));
+                localStorage.add(new Store(Helper.encrypt(url), 1));
             }
         } catch (Exception e) {
             return new ResponseEntity<String>("Some Exception", HttpStatus.OK);
@@ -34,28 +34,26 @@ public class ApiServiceImpl implements  ApiService {
         return new ResponseEntity<String>("Success", HttpStatus.OK);
     }
 
-    @Override
     public ResponseEntity<Store> getUrl(String url) {
+
         for (Store st : localStorage) {
-            if (st.getKey().equals(url)) {
+            if (Helper.decrypt(st.getKey()).equals(url)) {
                 return new ResponseEntity<Store>(st, HttpStatus.OK);
             }
         }
         return new ResponseEntity<Store>(new Store(), HttpStatus.OK);
     }
 
-    @Override
     public ResponseEntity<Integer> getCount(String url) {
 
         for (Store st : localStorage) {
-            if (st.getKey().equals(url)) {
+            if (Helper.decrypt(st.getKey()).equals(url)) {
                 return new ResponseEntity<Integer>(st.getCount(), HttpStatus.OK);
             }
         }
         return new ResponseEntity<Integer>(0, HttpStatus.OK);
     }
 
-    @Override
     public ResponseEntity<List<Store>> getList(int page, int size) {
 
         List<Store> data = new ArrayList<Store>();
--- src/main/java/com/itsambuja/nuchange/util/Helper.java
+++ src/main/java/com/itsambuja/nuchange/util/Helper.java
@@ -1,7 +1,65 @@
 package com.itsambuja.nuchange.util;
 
+import java.security.spec.KeySpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESedeKeySpec;
+
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Helper {
+
+    private static final String UNICODE_FORMAT = "UTF8";
+    public static final String DESEDE_ENCRYPTION_SCHEME = "DESede";
+    public static final String SECRET_KEY = "nuchangeassignmentbyitsambuja";
+    private static KeySpec ks;
+    private static SecretKeyFactory skf;
+    private static Cipher cipher;
+    static byte[] arrayBytes;
+    private static String myEncryptionKey;
+    private static String myEncryptionScheme;
+    static SecretKey key;
+
+    public Helper() throws Exception {
+        myEncryptionKey = SECRET_KEY;
+        myEncryptionScheme = DESEDE_ENCRYPTION_SCHEME;
+
+        arrayBytes = myEncryptionKey.getBytes(UNICODE_FORMAT);
+
+        ks = new DESedeKeySpec(arrayBytes);
+        skf = SecretKeyFactory.getInstance(myEncryptionScheme);
+        cipher = Cipher.getInstance(myEncryptionScheme);
+        key = skf.generateSecret(ks);
+    }
+
+    public static String encrypt(String unencryptedString) {
+        String encryptedString = null;
+        try {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            byte[] plainText = unencryptedString.getBytes(UNICODE_FORMAT);
+            byte[] encryptedText = cipher.doFinal(plainText);
+            encryptedString = new String(Base64.encodeBase64(encryptedText));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedString;
+    }
+
+
+    public static String decrypt(String encryptedString) {
+        String decryptedText=null;
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            byte[] encryptedText = Base64.decodeBase64(encryptedString);
+            byte[] plainText = cipher.doFinal(encryptedText);
+            decryptedText= new String(plainText);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return decryptedText;
+    }
 }
