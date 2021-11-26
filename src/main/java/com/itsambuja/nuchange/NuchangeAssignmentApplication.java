cc3fbde8a74424d9497f31d52ca9072582333268
AmbujaAK <itsambuja@gmail.com>
2021-11-27 00:34:34 +0530

    project setup

---
+++ src/main/java/com/itsambuja/nuchange/NuchangeAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.itsambuja.nuchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuchangeAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NuchangeAssignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itsambuja/nuchange/NuchangeAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsambuja.nuchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuchangeAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
