0f293807d44c17f331fe73ebaf4d26cfc2396559
AmbujaAK <itsambuja@gmail.com>
2021-11-27 01:18:52 +0530

    implemented storeurl & getUrl

--- src/main/java/com/itsambuja/nuchange/service/ApiServiceImpl.java
+++ src/main/java/com/itsambuja/nuchange/service/ApiServiceImpl.java
@@ -1,21 +1,47 @@
 package com.itsambuja.nuchange.service;
 
 import com.itsambuja.nuchange.model.Store;
+import com.itsambuja.nuchange.util.Helper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ApiServiceImpl implements  ApiService {
-    @Override
-    public ResponseEntity<String> storeUrl(String url) {
-        return null;
-    }
+    public static List<Store> localStorage = new ArrayList<Store>();
 
     @Override
+    public ResponseEntity<String> storeUrl(String url) {
+        boolean isNew = true;
+
+        try {
+            for (Store st : localStorage) {
+                if (st.getKey().equals(url)) {
+                    st.setCount(st.getCount() + 1);
+                    isNew = false;
+                }
+            }
+
+            if (isNew) {
+                localStorage.add(new Store(url, 1));
+            }
+        } catch (Exception e) {
+            return new ResponseEntity<String>("Some Exception", HttpStatus.OK);
+        }
+        return new ResponseEntity<String>("Success", HttpStatus.OK);
+    }
+    
+    @Override
     public ResponseEntity<Store> getUrl(String url) {
-        return null;
+        for (Store st : localStorage) {
+            if (st.getKey().equals(url)) {
+                return new ResponseEntity<Store>(st, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<Store>(new Store(), HttpStatus.OK);
     }
 
     @Override
